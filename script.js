c8b755af269646421dcb652146aca5aed6b675f9
juaniwk3 <juaniwk3@gmail.com>
2021-08-26 16:20:28 -0300

    Scoreboard, bugs

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 let sum1 = 0;
 let sum2 = 0;
-player1 = false
-player2 = false
+player1 = false;
+player2 = false;
 gameStarted = false;
 player1finished = false;
 player2finished = false;
+let player1wins = 0;
+let player2wins = 0;
 
 
 function start() {
@@ -23,12 +25,18 @@ function start() {
 
         if (player == 1) {
             player1 = true
+            player2 = false
             document.getElementById("turn1").textContent = "Your Turn"
+            document.getElementById("p1draw").style.border = "1px solid green";
+            document.getElementById("stop1").style.border = "1px solid green";
 
         }
         else if (player == 2) {
             player2 = true
+            player1 = false
             document.getElementById("turn2").textContent = "Your Turn"
+            document.getElementById("p2draw").style.border = "1px solid rgb(116, 220, 255)";
+            document.getElementById("stop2").style.border = "1px solid rgb(116, 220, 255)";
 
         }
 
@@ -42,14 +50,19 @@ function start() {
             document.getElementById("newcard2").textContent = "New card: 0"
             sum1 = 0
             sum2 = 0
-            document.getElementById("turn1").textContent = ""
+            document.getElementById("turn1").textContent = ""'
             document.getElementById("turn2").textContent = ""
             gameStarted = false;
             document.getElementById("start").textContent = "Start"
             player1finished = false;
             player2finished = false;
+            player = 0
             document.getElementById("msg1").textContent = ""
             document.getElementById("msg2").textContent = ""
+            document.getElementById("p2draw").style.border = "1px solid lightgray";
+            document.getElementById("stop2").style.border = "1px solid lightgray";
+            document.getElementById("p1draw").style.border = "1px solid lightgray";
+            document.getElementById("stop1").style.border = "1px solid lightgray";
         }
         finish()
     }
@@ -66,11 +79,24 @@ function draw1() {
         if (player2finished == false) {
             document.getElementById("turn1").textContent = ""
             document.getElementById("turn2").textContent = "Your Turn"
+            document.getElementById("p1draw").style.border = "1px solid lightgray";
+            document.getElementById("stop1").style.border = "1px solid lightgray";
+            document.getElementById("p2draw").style.border = "1px solid rgb(116, 220, 255)";
+            document.getElementById("stop2").style.border = "1px solid rgb(116, 220, 255)";
+
         }
     }
     if (sum1 > 21) {
         player1finished = true
         document.getElementById("msg1").textContent = "You're out"
+        alert("Player 2 won")
+        player2wins += 1;
+        document.getElementById("player2-wins").textContent = player2wins;
+    }
+    if (sum1 == 21) {
+        alert("Player 1 won")
+        player1wins += 1;
+        document.getElementById("player1-wins").textContent = player1wins;
     }
 }
 
@@ -85,11 +111,23 @@ function draw2() {
         if (player1finished == false) {
             document.getElementById("turn1").textContent = "Your Turn"
             document.getElementById("turn2").textContent = ""
+            document.getElementById("p1draw").style.border = "1px solid green";
+            document.getElementById("stop1").style.border = "1px solid green";
+            document.getElementById("p2draw").style.border = "1px solid lightgray";
+            document.getElementById("stop2").style.border = "1px solid lightgray";
         }
     }
     if (sum2 > 21) {
         player2finished = true
         document.getElementById("msg2").textContent = "You're out"
+        alert("Player 1 won")
+        player1wins += 1;
+        document.getElementById("player1-wins").textContent = player1wins;
+    }
+    if (sum1 == 21) {
+        alert("Player 2 won")
+        player2wins += 1;
+        document.getElementById("player2-wins").textContent = player2wins;
     }
 }
 
@@ -102,16 +140,21 @@ function stop1() {
     }
 
     if (player1finished == true && player2finished == true) {
-        if (sum1 > sum2) {
-            alert("Player 1 Wins");
+        if (sum1 > sum2 && sum1 < 21) {
+            alert("Player 1 Won");
+            player1wins += 1;
+            document.getElementById("player1-wins").textContent = player1wins;
         }
-        if (sum1 == sum2) {
+        else if (sum1 == sum2) {
             alert("The game ended with a draw")
         }
-        else {
-            alert("Player 2 Wins");
+        else if (sum2 > sum1 && sum2 < 21) {
+            alert("Player 2 Won");
+            player2wins += 1;
+            document.getElementById("player2-wins").textContent = player2wins;
         }
     }
+    document.getElementById("msg1").textContent = "Stopped"
 
 }
 
@@ -124,15 +167,19 @@ function stop2() {
     }
 
     if (player1finished == true && player2finished == true) {
-        if (sum1 > sum2) {
-            alert("Player 1 Wins");
+        if (sum1 > sum2 && sum1 < 21) {
+            alert("Player 1 Won");
+            player1wins += 1;
+            document.getElementById("player1-wins").textContent = player1wins;
         }
-        if (sum1 == sum2) {
+        else if (sum1 == sum2) {
             alert("The game ended with a draw")
         }
-        else {
-            alert("Player 2 Wins");
+        else if (sum2 > sum1 && sum2 < 21) {
+            alert("Player 2 Won");
+            player2wins += 1;
+            document.getElementById("player2-wins").textContent = player2wins;
         }
     }
-
+    document.getElementById("msg2").textContent = "Stopped"
 }
